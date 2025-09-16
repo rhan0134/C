9d40f899a30e4c820af38a3b63e7d65230a364fa
rhan0134 <yurhan.0134@gmail.com>
2025-09-16 09:58:23 +0900

    250916_C:C26/C++:C++34,C++35,C++36

---
+++ Programmers/C++35_conditionstr.c
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string ineq, string eq, int n, int m) {
+    int answer = 0;
+    
+    if(ineq == ">" && eq == "=" && n >= m)
+    {
+        answer = 1;
+    }else if(ineq == ">" && eq == "!" && n > m)
+    {
+        answer = 1;
+    }else if(ineq == "<" && eq == "=" && n <= m)
+    {
+        answer = 1;
+    }else if(ineq == "<" && eq == "!" && n < m)
+    {
+        answer = 1;
+    }else
+    {
+        answer = 0;
+    }
+    return answer;
+}
